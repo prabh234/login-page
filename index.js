73d5a0e44df6648627a4fabec290a1a8c350c531
prabhjot singh <48572962+prabh234@users.noreply.github.com>
2024-02-09 11:11:28 +0530

    updated with signup

---
+++ index.js
@@ -0,0 +1,13 @@
+function signup() {
+ 
+document.getElementById("login").style.display = "none";
+document.getElementById("signup").style.display = "flex";
+   
+}
+
+function login() {
+ 
+document.getElementById("login").style.display = "flex";
+document.getElementById("signup").style.display = "none";
+   
+}
